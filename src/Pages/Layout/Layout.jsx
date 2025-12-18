a5c4c4fd5a4ccc84932387787c699fb85bf2cff2
Hagar <tgyg734@gmail.com>
2025-12-18 18:27:12 +0200

    layout page

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -1,1 +1,16 @@
-//dgdgd
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../Components/Footer/Footer';
+
+function Layout({toggelTheme , theme}) {
+    return (
+        <div className='dark:bg-gray-500'>
+            <Navbar  toggelTheme={toggelTheme} theme={theme}/>
+            <Outlet/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Layout;
