245a67ebc2138993961cf4f8e464c2d51ca352be
Hagar <tgyg734@gmail.com>
2025-12-18 18:36:12 +0200

    loader page

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,1 +1,11 @@
-//loader
+import React from 'react';
+
+function Loader() {
+    return (
+        <div className='w-full min-h-80 flex items-center-safe'> 
+            <span className="loader dark:bg-white"></span>
+        </div>
+    );
+}
+
+export default Loader;
