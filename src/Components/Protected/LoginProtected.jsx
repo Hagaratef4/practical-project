6cd31d9451aa208d0bcd6b43ed0e83f22f25eb43
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2025-12-18 17:17:48 +0200

    feat(auth): add LoginProtected and ProtectedRoutes guards

--- src/Components/Protected/LoginProtected.jsx
+++ src/Components/Protected/LoginProtected.jsx
@@ -1,1 +1,16 @@
-//lohin
+import React, { useContext } from 'react';
+import { authContext } from '../../Context/AuthContext';
+import { Navigate } from 'react-router-dom';
+
+function LoginProtected({ children }) {
+
+    let { token } = useContext(authContext)
+
+    return (
+        <div>
+            {!token ? children : <Navigate to={'/'} />}
+        </div>
+    );
+}
+
+export default LoginProtected;
--- src/Components/Protected/ProtectedRoutes.jsx
+++ src/Components/Protected/ProtectedRoutes.jsx
@@ -1,1 +1,16 @@
-//login
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { authContext } from '../../Context/AuthContext';
+
+function ProtectedRoutes({ children }) {
+
+    let { token } = useContext(authContext)
+
+    return (
+        <div>
+            {token ? children : <Navigate to={'/login'} />}
+        </div>
+    );
+}
+
+export default ProtectedRoutes;
