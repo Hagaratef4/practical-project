6c6c359d01023ff3c8a0fdd9bb2f00b79a3b4236
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2025-12-18 16:55:38 +0200

    feat: implement authentication context with token verification.

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,1 +1,45 @@
-//auth
+import axios from "axios";
+import {createContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+
+export let authContext = createContext(null)
+
+
+
+function AuthContextProvider({children}) {
+
+    async function verifyToken(){
+       
+        if(localStorage.getItem('token')){
+
+             try{
+            let {data} = await axios.get('https://ecommerce.routemisr.com/api/v1/auth/verifyToken' , {
+                headers :{
+                    token : localStorage.getItem('token'),
+                },
+            })
+            localStorage.setItem('userId',data.decoded.id)
+        }catch(err) {
+            console.error(err)
+            toast.error("Please login to get access")
+            setToken(null)
+            localStorage.removeItem('token')
+        }
+        }
+    }
+
+    useEffect(()=>{
+        verifyToken()
+    },[])
+
+    let [token , setToken] = useState(localStorage.getItem('token'))
+
+    return (
+        <authContext.Provider value={{token , setToken , verifyToken}}>
+            {children}
+        </authContext.Provider>
+    );
+}
+
+export default AuthContextProvider;
